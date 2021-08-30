36a64be67c9370de3712349db44b0bd8301942a7
Jacobsky <cael.jacobsen@gmail.com>
2021-08-30 11:03:50 +0900

    First version of the colors with the ability to configure the HTML color of each property type.
    Some minor tweaks to the spacing UI.

--- src/views/colors.rs
+++ src/views/colors.rs
@@ -1,24 +1,49 @@
 use eframe::egui::{Style, Ui, Grid, widgets, Color32, Response};
 
 // TODO: Use this to differentiate
-pub enum ColorEditType {
-    RGB, HSV, RGBA, SRGBA
-}
+// pub enum ColorEditType {
+//     RGB, HSV, RGBA, SRGBA
+// }
 
 /// Displays and modifies the style information related to color for the application.
 pub fn colors_view(style: &mut Style, ui: &mut Ui) {
+    ui.heading("Color Settings");
+    // macro_rules! color_grid_for_settings
+    /// This is a convenience macro for building out this specific table structure without the additional boilerplate.
     macro_rules! add_color_grid_rows {
         ($ui:expr, $($label_color:expr),*) => {
+            $ui.label("Property");
+            $ui.label("Color Picker");
+            $ui.label("Html Color Value");
+            $ui.end_row();
             $(
                 $ui.label($label_color.0);
                 $ui.color_edit_button_srgba($label_color.1);
+                let mut html_color = color_to_html(&$label_color.1);
+                let response = $ui.text_edit_singleline(&mut html_color);
+                if response.changed() || response.lost_focus() {
+                    if let Some(color) = html_to_color(html_color.as_str()) {
+                        *$label_color.1 = color;
+                    } 
+                }
                 $ui.end_row();
             )*
         }
     }
-    Grid::new("color_settings").num_columns(2).show(ui, | ui |{
-        ui.heading("General");
+    Grid::new("color_settings").num_columns(3).show(ui, | ui |{
+        ui.heading("General Color Settings");
+        
         ui.end_row();
+        // ui.label("test color");
+        // ui.color_edit_button_srgba(&mut style.visuals.extreme_bg_color);
+        // let mut html_color = color_to_html(&style.visuals.extreme_bg_color);
+        // let response = ui.text_edit_singleline(&mut html_color);
+        // if response.changed() || response.lost_focus() {
+        //     if let Some(color) = html_to_color(html_color.as_str()) {
+        //         style.visuals.extreme_bg_color = color;
+        //     } 
+        // }
+        // ui.end_row();
         add_color_grid_rows!(ui,
             ("Background Faint", &mut style.visuals.faint_bg_color),
             ("Background Extreme", &mut style.visuals.extreme_bg_color),
@@ -72,11 +97,49 @@ pub fn colors_view(style: &mut Style, ui: &mut Ui) {
     });
 }
 
-fn edit_color(ui: &mut Ui, srgba: &mut Color32, text: impl Into<widgets::Label>) -> Response {
-    // ui.horizontal(|ui| {
-        let response = ui.color_edit_button_srgba(srgba);
-        ui.label(text);
-        response
-    // })
-    // .response
+
+fn color_to_html(color: &Color32) -> String {
+    format!("#{:02x}{:02x}{:02x}{:02x}", color.r(), color.g(), color.b(), color.a())
+}
+
+// Returns the html color if it is able to or None if it cannot convert it.
+fn html_to_color(html: &str) -> Option<Color32> {
+    use std::convert::TryFrom;
+    let mut html = html.chars();
+    // HTML Color codes require exactly 9 characters (hash + 8 digits).
+    // We will also support 6 where we assume that alpha is 255.
+    if let Some(char) = html.next() {
+        if char != '#' {
+            return None;
+        }
+    }
+    let red = if let (Some(red1), Some(red2)) = (html.next(), html.next()) {
+        if let (Some(r1), Some(r2)) = (red1.to_digit(16), red2.to_digit(16)) {
+            Some(r1 * 16 + r2)
+        } else { None }
+    } else { None };
+    let green = if let (Some(green1), Some(green2)) = (html.next(), html.next()) {
+        if let (Some(g1), Some(g2)) = (green1.to_digit(16), green2.to_digit(16)) {
+            Some(g1 * 16 + g2)
+        } else { None }
+    } else { None };
+    let blue = if let (Some(blue1), Some(blue2)) = (html.next(), html.next()) {
+        if let (Some(b1), Some(b2)) = (blue1.to_digit(16), blue2.to_digit(16)) {
+            Some(b1 * 16 + b2)
+        } else { None }
+    } else { None };
+    let alpha = if let (Some(alpha1), Some(alpha2)) = (html.next(), html.next()) {
+        if let (Some(a1), Some(a2)) = (alpha1.to_digit(16), alpha2.to_digit(16)) {
+            Some(a1 * 16 + a2)
+        } else { None }
+    } else { Some(255) };
+    if let (Some(r), Some(b), Some(g), Some(a)) = (red, green, blue, alpha) {
+        if let (Ok(r), Ok(b), Ok(g), Ok(a)) = (u8::try_from(r), u8::try_from(g), u8::try_from(b), u8::try_from(a)) {
+            Some(Color32::from_rgba_premultiplied(r, g, b, a))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
 }
--- src/views/spacing.rs
+++ src/views/spacing.rs
@@ -2,5 +2,6 @@ use eframe::egui::{Style, Ui};
 
 /// Displays the Ui to help modify and calculate the current spacing information.
 pub fn spacing_view(style: &mut Style, ui: &mut Ui) {
+    ui.heading("Spacing Configuration");
     style.spacing.ui(ui)
 }
