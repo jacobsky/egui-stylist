8fac22d917ef52e22bcbbded0a5180c1f8408e04
Jacobsky <jacobsky@users.noreply.github.com>
2022-05-30 21:46:01 +0900

    Added test cases for font data
    Included Nacelle-Nacelle-Regular as a test font.
    Fixed bugs related to font family serialization.

--- egui-theme/src/tests/ser.rs
+++ egui-theme/src/tests/ser.rs
@@ -1,3 +1,5 @@
+use egui::FontData;
+
 use crate::EguiTheme;
 #[test]
 fn test_serialize_theme() {
@@ -28,4 +30,45 @@ fn test_default_theme_extract() {
         let serialized_font = default_fonts.font_data.get(*name).expect("{name} does not exist");
         assert_eq!(default_font, serialized_font);
     }
+}
+
+#[test]
+fn test_custom_font_extract() {
+    const FONT_NAME: &str = "Nacelle";
+    let mut font_definitions = egui::FontDefinitions::default();
+    font_definitions.font_data.insert(FONT_NAME.to_owned(), FontData::from_static(include_bytes!("test-fonts/Nacelle-Regular.otf")));
+    let result = font_definitions.families.insert(egui::FontFamily::Name(FONT_NAME.into()), vec![FONT_NAME.to_owned()]);
+    assert_eq!(result, None, "None was not returned");
+    
+    let theme = EguiTheme::new(
+        egui::Style::default(),
+        font_definitions
+    );
+
+    let (_, fonts) = theme.extract();
+    assert!(fonts.font_data.contains_key(&FONT_NAME.to_owned()), "does not have `Nacelle` key");
+    assert_eq!(fonts.font_data.get(&FONT_NAME.to_owned()).unwrap(), &FontData::from_static(include_bytes!("test-fonts/Nacelle-Regular.otf")), "font data does not match");
+    assert!(fonts.families.contains_key(&egui::FontFamily::Monospace), "does not have the `Monospace` family");
+    assert!(fonts.families.contains_key(&egui::FontFamily::Proportional), "does not have the `Proportional` family");
+    assert!(fonts.families.contains_key(&egui::FontFamily::Name(FONT_NAME.into())), "does not have the `Nacelle` family");
+    assert_eq!(fonts.families.get(&egui::FontFamily::Name(FONT_NAME.into())).unwrap(), &vec![FONT_NAME.to_owned()], "`Nacelle` family does not have the correct data");
+}
+
+#[test]
+fn test_custom_font_serialization() {
+    const FONT_NAME: &str = "Nacelle";
+    let mut font_definitions = egui::FontDefinitions::default();
+    font_definitions.font_data.insert(FONT_NAME.to_owned(), FontData::from_static(include_bytes!("test-fonts/Nacelle-Regular.otf")));
+    font_definitions.families.insert(egui::FontFamily::Name(FONT_NAME.into()), vec![FONT_NAME.to_owned()]);
+
+    let theme = EguiTheme::new(
+        egui::Style::default(),
+        font_definitions
+    );
+
+    let serialized = serde_json::to_string(&theme).expect("serialization failed");
+
+    assert!(serialized.contains("Nacelle"), "Nacelle not found in the serialized string");
+    let nacelle_font_data = serde_json::to_value(FontData::from_static(include_bytes!("test-fonts/Nacelle-Regular.otf"))).unwrap();
+    assert!(serialized.contains(&nacelle_font_data.to_string()), "Nacelle data does not exist in the serialized string");
 }
--- egui-theme/src/theme/fonts.rs
+++ egui-theme/src/theme/fonts.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, BTreeMap};
 use egui::FontDefinitions;
 use egui::FontData;
+use egui::FontFamily;
 
 const FONT_DATA_KEY: &str = "font_data";
 const FAMILIES_KEY: &str = "families";
@@ -17,44 +18,58 @@ fn remove_default_fonts<'a>(mut font_data: BTreeMap<String, FontData>) -> BTreeM
 /// TODO: Comment this function
 pub fn from_fonts(FontDefinitions { font_data, families }: FontDefinitions) -> HashMap<String, serde_json::Value> {
     let font_data = remove_default_fonts(font_data);
-    let default_defs = FontDefinitions::default();
     let mut hash_map = HashMap::new();
+
     hash_map.insert(
         FONT_DATA_KEY.to_owned(),
         serde_json::to_value(font_data)
-        .unwrap_or_else(|_err| {
-            serde_json::to_value(default_defs.font_data.to_owned())
-            .expect("the fallback should work")
-        })
+        .expect("serialization error occurred")
     );
     
-    /// TODO: Comment this function
+    println!("{:?}", &families);
+    // Workaround due to FontFamily not properly serializing to "String" when attempting to serialize the BTreeMap<FontFamily, Vec<String>>
+    let families= {
+        families
+            .iter()
+            .map(|(family, list)| {
+                (serde_json::to_string(family).expect("serialization failed"), list.clone())
+            })
+            .collect::<Vec<_>>()
+    };
     hash_map.insert(
         FAMILIES_KEY.to_owned(),
         serde_json::to_value(families)
-            .unwrap_or_else(|_err| {
-                serde_json::to_value(default_defs.families.to_owned())
-                .expect("the fallback should work")
-            })
+            .expect("serialization error occurred")
     );
-
+    
+    println!("{:?}", hash_map.get(FAMILIES_KEY));
     hash_map
 }
 
+/// TODO: Comment this function
 pub fn to_fonts(hash_map: HashMap<String, serde_json::Value>) -> FontDefinitions {
     let mut fonts = FontDefinitions::default();
     
     hash_map.get(&FONT_DATA_KEY.to_owned()).map(|value| {
-        let font_data= serde_json::from_value::<BTreeMap<String, FontData>>(value.to_owned()).unwrap_or_default();
-        for (k, v) in font_data.iter() {
-            let _ = fonts.font_data.insert(k.to_owned(), v.to_owned());
+        if let Ok(font_data) = serde_json::from_value::<BTreeMap<String, FontData>>(value.to_owned()) {
+            for (k, v) in font_data.iter() {
+                let _ = fonts.font_data.insert(k.to_owned(), v.to_owned());
+            }
         }
     });
 
     hash_map.get(&FAMILIES_KEY.to_owned()).map(|value| {
-        fonts.families = serde_json::from_value(value.to_owned()).unwrap_or_default();
+        // Workaround due to FontFamily not properly serializing to "String" when attempting to serialize the BTreeMap<FontFamily, Vec<String>>
+        if let Ok(families) = serde_json::from_value::<Vec<(String, Vec<String>)>>(value.to_owned()) {
+            // println!("{:?}", families);
+            for (family, list) in families {
+                let family = serde_json::from_str(family.as_str()).expect("serialization failed");
+                fonts.families.insert(
+                    family, list.clone()
+                );
+            }
+        };
     });
-    // Text fonts does not have a default, so it has to be implemented manually instead of being macroed.
         
     fonts
 }
